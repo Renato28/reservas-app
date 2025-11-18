a0700e6a315d5ad1e8acc6af1f6066a83d778865
Renato Nobrega <renatonobrega87@gmail.com>
2025-11-18 08:16:57 -0300

    criando rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './core/componentes/login.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { ForgotPasswordComponent } from './core/auth/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './core/auth/reset-password/reset-password.component';
+import { RegisterUsuarioComponent } from './core/auth/register-usuario/register-usuario.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'reset-password', component: ResetPasswordComponent },
+  { path: 'register', component: RegisterUsuarioComponent},
 
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
