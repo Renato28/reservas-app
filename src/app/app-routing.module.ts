61bdf95143786d4f1d13b472b2b35b60af03d70f
Renato Nobrega <renatonobrega87@gmail.com>
2025-11-17 22:47:35 -0300

    adicionando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './core/componentes/login.component';
 import { AuthGuard } from './core/guards/auth.guard';
+import { ForgotPasswordComponent } from './core/auth/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './core/auth/reset-password/reset-password.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-
-  {
-    path: 'dashboard',
-    canActivate: [AuthGuard],
-    loadChildren: () =>
-      import('./modules/dashboard/dashboard.module')
-        .then(m => m.DashboardModule)
-  },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
 
   { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' }
 ];
 
 
