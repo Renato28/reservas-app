22ccccbd8117a692498f55ea6b634a74d2357c6d
Renato Nobrega <renatonobrega87@gmail.com>
2025-11-22 14:24:54 -0300

    criando o service para adicionar hospedes a reserva

---
+++ src/app/core/services/hospede-reserva/hospede-reserva.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HospedeReservaRequest } from '../../models/hospede-reserva-request';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HospedeReservaService {
+
+  private apiUrl = 'http://localhost:8080/api/hospedes'
+
+  constructor(private http: HttpClient) { }
+
+  adicionar(id: number, dto: HospedeReservaRequest): Observable<any> {
+     return this.http.post<any>(`${this.apiUrl}/hospedes/${id}`, dto);
+  }
+}
