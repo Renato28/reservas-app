d8f034d3acbba7a5c90a236ec34cbf502c90a793
Renato Nobrega <renatonobrega87@gmail.com>
2025-11-23 11:10:01 -0300

    atualizando as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './core/componentes/login.component';
-import { AuthGuard } from './core/guards/auth.guard';
+import { LoginComponent } from './core/componentes/login/login.component';
 import { ForgotPasswordComponent } from './core/auth/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './core/auth/reset-password/reset-password.component';
 import { RegisterUsuarioComponent } from './core/auth/register-usuario/register-usuario.component';
+import { DashboardComponent } from './core/componentes/dashboard/dashboard.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule), canActivate: [AuthGuard] },
+
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+
+  { path: '**', redirectTo: 'dashboard' },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'reset-password', component: ResetPasswordComponent },
   { path: 'register', component: RegisterUsuarioComponent},
 
   { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'dashboard', loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule) },
   { path: '**', redirectTo: 'login' }
 ];
 
