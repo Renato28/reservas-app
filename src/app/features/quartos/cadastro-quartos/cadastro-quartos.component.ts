43f7c5a8991ae08dfad5849a0dfad317e1e3d14d
Renato Nobrega <renatonobrega87@gmail.com>
2025-11-24 21:08:33 -0300

    criando component de cadastro de quartos

---
+++ src/app/features/quartos/cadastro-quartos/cadastro-quartos.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { QuartoRequest } from 'src/app/core/models/quarto-request.model';
+import { StatusQuarto } from 'src/app/core/models/status-quarto.enum';
+import { TipoQuarto } from 'src/app/core/models/tipo-quarto.enum';
+import { QuartoService } from 'src/app/core/services/quarto/quarto.service';
+
+@Component({
+  selector: 'app-cadastro-quartos',
+  templateUrl: './cadastro-quartos.component.html',
+  styleUrls: ['./cadastro-quartos.component.css']
+})
+export class CadastroQuartosComponent implements OnInit {
+
+  quartoForm!: FormGroup;
+  submitting = false;
+
+  tiposQuarto = Object.values(TipoQuarto);
+  statusOptions = Object.values(StatusQuarto);
+
+  constructor(
+    private fb: FormBuilder,
+    private quartoService: QuartoService,
+    private toastService: ToastrService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.quartoForm = this.fb.group({
+      numero: ['', Validators.required],
+      tipo: ['', Validators.required],
+      precoDiaria: ['', [Validators.required, Validators.min(1)]],
+      status: ['', Validators.required]
+    });
+  }
+
+  invalid(field: string) {
+    const control = this.quartoForm.get(field);
+    return control?.invalid && control?.touched;
+  }
+
+  cadastrar() {
+    if (this.quartoForm.invalid) {
+      this.quartoForm.markAllAsTouched();
+      return;
+    }
+
+    this.submitting = true;
+
+    const quarto: QuartoRequest = this.quartoForm.value;
+
+    this.quartoService.cadastrar(quarto).subscribe({
+      next: () => {
+        this.toastService.success('Quarto cadastrado com sucesso!');
+        this.router.navigate(['/quartos']);
+      },
+      error: () => {
+        this.toastService.error('Erro ao cadastrar o quarto');
+        this.submitting = false;
+      }
+    });
+  }
+
+  get f() {
+    return this.quartoForm.controls;
+  }
+
+}
